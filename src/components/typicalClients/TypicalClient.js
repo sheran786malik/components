9d8a4e09da502e0caf6c4bb04ab349388cdda02a
Sheran <sheran_malik@hotmail.co.uk>
2018-12-28 01:46:40 +0000

    First commit

--- src/components/typicalClients/TypicalClient.js
+++ src/components/typicalClients/TypicalClient.js
@@ -1,31 +1,51 @@
 import React from 'react';
 
-import Data from '../data/Data';
+
 
 import './TypicalClient.css';
 
 const TypicalClient = (props) => {
 
-    console.log(props);
+    const client = [
+        'ClientName'
+    ];
+
+    let clientName = '';
+
+    const title = [
+        'Rental Property Income',
+        'Digital Nomad'
+    ]
+    let inText = '';
+
+    const subText = [
+        'We specialise in mortgages for the self-employed. We offer help and advice, whatever your situation.',
+        'We specialise in mortgages for the self-employed. We offer help and advice, whatever your situation.'
+    ]
+
+
+
+    let bodyText = '';
+
+
+    clientName = client[props.clientName];
+    inText= title[props.inText];
+    bodyText = subText[props.bodyText];
 
     return (
     <div className="typicalClient">
 
         <div className="title">
             <h1 className="clientName">
-                <Data 
-                clientName='0'
-                />
+                {client[0]}
             </h1>
             <h2 className="subtitle">
-                <Data 
-                SectionIndex='0'
-                />
+                {title[0]}
             </h2>
         </div>
-            <div className="typicalText">
-                <Data subIndex='1'/>
-            </div>
+            <h1 className="typicalText">
+                {subText[0]}
+            </h1>
     </div>
     
     
